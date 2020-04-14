7fd1e77dcae40c817d8172fda3d04bcca01d6b7a
Jon Cram <webignition@gmail.com>
2020-04-14 11:37:52 +0100

    Import QuotedValueExtractor (#9)

---
+++ tests/DataProvider/QuotedValueDataProviderTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace webignition\BasilValueExtractor\Tests\DataProvider;
+
+trait QuotedValueDataProviderTrait
+{
+    public function quotedValueDataProvider(): array
+    {
+        return [
+            'quoted value: quoted string' => [
+                'valueString' => '"value"',
+                'expectedValue' => '"value"',
+            ],
+            'quoted value: quoted string lacking final quote' => [
+                'valueString' => '"value',
+                'expectedValue' => '"value"',
+            ],
+            'quoted value: quoted string with trailing data' => [
+                'valueString' => '"value" trailing',
+                'expectedValue' => '"value"',
+            ],
+            'quoted value: quoted string with escaped quotes' => [
+                'valueString' => '"\"value\""',
+                'expectedValue' => '"\"value\""',
+            ],
+            'quoted value: quoted string with escaped quotes with trailing data' => [
+                'valueString' => '"\"value\"" trailing',
+                'expectedValue' => '"\"value\""',
+            ],
+        ];
+    }
+}
